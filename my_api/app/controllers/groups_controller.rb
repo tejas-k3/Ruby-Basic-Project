464129dfc8e8112191c48449521c07f4a91a6402
Tejas <tejaskothari03@gmail.com>
2024-03-07 09:57:47 +0530

    Updated group Active Record

--- my_api/app/controllers/groups_controller.rb
+++ my_api/app/controllers/groups_controller.rb
@@ -10,8 +10,13 @@ class GroupsController < ApplicationController
 
   # GET /groups/1
   def show
-    render json: @group
+    @group = Group.find(params[:id])
+  
+    if stale?(last_modified: @group.updated_at)
+      render json: @group
+    end
   end
+  
 
   # POST /groups
   def create
@@ -35,12 +40,13 @@ class GroupsController < ApplicationController
 
   # DELETE /groups/1
   def destroy
-    @group.destroy!
+    group.destroy!
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_group
+    # 
+    def group
       @group = Group.find(params[:id])
     end
 
--- my_api/config/application.rb
+++ my_api/config/application.rb
@@ -28,5 +28,8 @@ module MyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.action_dispatch.rack_cache = true
+
   end
 end
