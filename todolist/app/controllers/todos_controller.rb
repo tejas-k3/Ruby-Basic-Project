719b2082cfa1f737ac61e4c11bd8583d871b543e
Tejas <tejaskothari03@gmail.com>
2024-03-10 15:25:11 +0530

    Created basic Json based CRUD APIs for Todo Resource
    Used AR cmd :
    ```
    Todo.create([
      { name: 'Go grocery shopping', description: 'Buy fruits and vegetables', state: 'created' },
      { name: 'Write blog post', description: 'Share insights on recent project', state: 'created' }
    ])
    ```

--- todolist/app/controllers/todos_controller.rb
+++ todolist/app/controllers/todos_controller.rb
@@ -1,3 +1,45 @@
 class TodosController < ApplicationController
+    # before_action :todo, only: [:show, :edit, :update, :destroy]
+    before_action :todo, only: [:show, :update]
+  
+    def index
+        @todos = Todo.all
+        render json: @todos
+    end
     
+    def show
+      render json: @todo
+    end
+  
+    def create
+      @todo = Todo.new(todo_params)
+      if @todo.save
+        render json: @todo, status: :created
+      else
+        render json: @todo.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @todo.update(todo_params)
+        render json: @todo
+      else
+        render json: @todo.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+        @todo.destroy
+        head :no_content
+    end
+
+    private
+  
+    def todo
+      @todo = Todo.find(params[:id])
+    end
+  
+    def todo_params
+      params.require(:todo).permit(:name, :description, :state)
+    end
 end
