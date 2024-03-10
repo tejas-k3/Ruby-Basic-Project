8fab8ff5814065bd6339c05fb7f5f00646e58ced
Tejas <tejaskothari03@gmail.com>
2024-03-10 16:36:32 +0530

    Added health check which checks DB connection

--- todolist/app/controllers/application_controller.rb
+++ todolist/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def health_check
+        if ActiveRecord::Base.connection.active?
+          render json: { status: 'OK' }, status: :ok
+        else
+          render json: { error: 'Database connection is not active' }, status: :service_unavailable
+        end
+      end
 end
