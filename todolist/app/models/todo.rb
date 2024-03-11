67f555f6aafa081b342a8f08c8b518a4c202d3d6
Tejas <tejaskothari03@gmail.com>
2024-03-11 12:06:48 +0530

    Added polycomment
    Command :
    `rails g model PolyComment content:text
    commentable:references{polymorphic}`

--- todolist/app/models/todo.rb
+++ todolist/app/models/todo.rb
@@ -1,4 +1,5 @@
 class Todo < ApplicationRecord
     enum state: { created: 0, in_progress: 1, done: 2 }
     has_one_attached :image
+    has_many :poly_comments, as: :commentable
 end
--- todolist/db/schema.rb
+++ todolist/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_10_182037) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_050943) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_182037) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "poly_comments", force: :cascade do |t|
+    t.text "content"
+    t.string "commentable_type", null: false
+    t.integer "commentable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_poly_comments_on_commentable"
   end
 
   create_table "todos", force: :cascade do |t|
